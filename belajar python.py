6fc5d5016a93780327eafbcbfa761b2fb3d417f1
Xezano <119835591+Xezano@users.noreply.github.com>
2023-03-26 22:38:09 -0700

    Add files via upload

---
+++ belajar python.py
@@ -0,0 +1,47 @@
+# my_list = ["A","B", 1,2]
+
+# print("a" in my_list) #output true
+# print("c" in my_list) #output true
+# print(2 not in my_list) #output false
+
+# #tampilkan output
+# my_list
+# print("list untuk nomor 1 adalah : ", my_list[1])
+
+#====================================================================================#
+
+# a = 3.0
+# c = a ** 2
+# print("nilai c", c)
+# c = a ** 4
+# print("nilai c", c)
+
+#====================================================================================#
+
+# a = 3.0
+# b = 4.0
+# c = (a ** 2 + b ** 2) ** 0.5
+# print("c = ", c)
+
+#====================================================================================#
+
+#Quiz Kecil
+#buatlah program python
+#rumus luas = Phi * r^2
+
+# Phi = 3.14
+# r = float(input("Masukan Panjang jari-jari lingkaran : "))
+# luas = Phi*r*r
+# print("Luas Lingkaran adalah : ",luas)
+
+#====================================================================================#
+
+#Read two numbers
+# number1 = int()
+
+#====================================================================================#
+
+#contoh loop
+#buatlah angka loop dari angka 2000 - 2010
+# for i in range(10):
+#     print("angka dengan nomor urut = [",i,"]")
